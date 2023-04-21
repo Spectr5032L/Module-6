f6383791965cdc12c3f979277f12b91852852c24
Spectr5032L <101048182+Spectr5032L@users.noreply.github.com>
2023-04-21 21:53:07 +0500

    Добавлена таблица и кнопки
    Добавлена таблица с возможностью изменять стороны, также на будущий функционал добавлены кнопки.

--- A star.js
+++ A star.js
@@ -1,10 +1,16 @@
-const n = 5; // Задаем размер квадратной карты
+function createTable() {
+    let n = document.getElementById("input").value;
+    let container = document.getElementById("tablecontainer");
+    let table = document.createElement("table");
 
-for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-        const square = document.createElement('div'); // Создаем элемент-квадрат
-        square.classList.add('square'); // Добавляем класс "square" для применения стилей
-        document.body.appendChild(square); // Добавляем квадрат на страницу
+    for (let i = 0; i < n; i++) {
+      let row = document.createElement("tr");
+      for (let j = 0; j < n; j++) {
+        let cell = document.createElement("td");
+        row.appendChild(cell);
+      }
+      table.appendChild(row);
     }
-
-}
+    container.innerHTML = "";
+    container.appendChild(table);
+  }
