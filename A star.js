744df53743901b825bd9103ca1be50f2671e0779
h-idwidwi <131362344+h-idwidwi@users.noreply.github.com>
2023-04-22 13:20:41 +0500

    because...

--- A star.js
+++ A star.js
@@ -4,25 +4,28 @@ function createTable() {
     let table = document.createElement("table");
 
     for (let i = 0; i < n; i++) {
-      let row = document.createElement("tr");
-      for (let j = 0; j < n; j++) {
-        let cell = document.createElement("td");
-        row.appendChild(cell);
-      }
-      table.appendChild(row);
+        let row = document.createElement("tr");
+        for (let j = 0; j < n; j++) {
+            let cell = document.createElement("td");
+            cell.onclick = function() {
+                cell.style.backgroundColor = "green";
+            }
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
     }
 
     // Появление кнопок
     var buttons = document.querySelectorAll(".Show");
     buttons.forEach(function(button) {
-      button.style.display = "inline-block";
+        button.style.display = "inline-block";
     });
 
     container.innerHTML = "";
     container.appendChild(table);
-  }
+}
 
-  function clearAll() {
+function clearAll() {
     location.reload();
 
-  }
+}
