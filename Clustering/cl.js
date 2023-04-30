29f3ea267743c9f606eaa64b9e879979c16e213f
Me-Ri <131341458+Me-Ri@users.noreply.github.com>
2023-04-30 20:21:11 +0500

    расстановка точек

--- Clustering/cl.js
+++ Clustering/cl.js
@@ -1,7 +1,21 @@
 let canvas = document.getElementById("canvas");
 let canva = canvas.getContext("2d");
+let points = [];
 
 function clear_canvas()
 {
     location.reload();
 }
+
+canvas.addEventListener('click', function(event)
+{
+    let x = event.pageX - canvas.offsetLeft;
+    let y = event.pageY - canvas.offsetTop;
+    points.push({x: x, y: y});
+
+    let size_point = document.getElementById("size_point").value;
+    canva.beginPath();
+    canva.arc(points[points.length - 1].x, points[points.length - 1].y, size_point, 0, 2 * Math.PI);
+    canva.fillStyle = "darkslategray";
+    canva.fill();
+});
