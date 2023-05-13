4f6f2864432fe98ac15ac5d3a74308560bd3a245
Me-Ri <131341458+Me-Ri@users.noreply.github.com>
2023-05-13 15:32:57 +0500

    Куча классных изменений

--- cl.js
+++ cl.js
@@ -1,18 +1,28 @@
 let canvas = document.getElementById("canvas");
-let canva = canvas.getContext("2d");
-let points = [];
-let points2 = [];
+let cnvs = canvas.getContext("2d");
+let pointsKMeans = [];
+let pointsDBSCAN = [];
 let clusters = [];
-line();
+
 
 function line()
 {
-    canva.beginPath();
-    canva.lineWidth = 2;
-    canva.moveTo(700, 0);
-    canva.lineTo(700, 700);
-    canva.strokeStyle = "#b803f9";
-    canva.stroke();
+    cnvs.beginPath();
+    cnvs.lineWidth = 2;
+    cnvs.moveTo(700, 0);
+    cnvs.lineTo(700, 700);
+    cnvs.strokeStyle = "#b803f9";
+    cnvs.stroke();
+}
+line();
+
+function receiptSizePoint()
+{
+    let sizePoint = document.getElementById("sizePoint").value;  
+    if(sizePoint < 3)
+        sizePoint = 3;
+
+    return sizePoint;
 }
 
 function clear_canvas()
@@ -22,30 +32,27 @@ function clear_canvas()
 
 function random_points()
 {
-    let count_points = document.getElementById("count_points").value;
+    let countPoints = document.getElementById("countPoints").value;
 
-    for(let i = 0; i < count_points; i++) 
+    for(let i = 0; i < countPoints; i++) 
     {
         let x = (Math.random() + 0.015) * 680;
         let y = Math.random() * canvas.height;
 
-        points.push({x: x, y: y});
-        points2.push({x: x + 700, y: y});
+        pointsKMeans.push({x: x, y: y});
+        pointsDBSCAN.push({x: x + 700, y: y});
 
-        canva.clearRect(0, 0, canvas.width, canvas.height);
+        cnvs.clearRect(0, 0, canvas.width, canvas.height);
         line();
-        for(let i = 0; i < points.length; i++) 
+        
+        for(let i = 0; i < pointsKMeans.length; i++) 
         {
-            let size_point = document.getElementById("size_point").value;  
-            canva.beginPath();
-            canva.arc(points[i].x, points[i].y, size_point, 0, 2 * Math.PI);
-            canva.fillStyle = "darkslategray";
-            canva.fill();
-
-            canva.beginPath();
-            canva.arc(points[i].x + 700, points[i].y, size_point, 0, 2 * Math.PI);
-            canva.fillStyle = "darkslategray";
-            canva.fill();
+            let sizePoint = receiptSizePoint();
+            cnvs.beginPath();
+            cnvs.arc(pointsKMeans[i].x, pointsKMeans[i].y, sizePoint, 0, 2 * Math.PI);
+            cnvs.arc(pointsKMeans[i].x + 700, pointsKMeans[i].y, sizePoint, 0, 2 * Math.PI);
+            cnvs.fillStyle = "darkslategray";
+            cnvs.fill();
         }
     }
 }
@@ -57,82 +64,78 @@ canvas.addEventListener('click', function(event)
 
     if(x <= 700)
     {
-        points.push({x: x, y: y});
-        points2.push({x: x + 700, y: y});
+        pointsKMeans.push({x: x, y: y});
+        pointsDBSCAN.push({x: x + 700, y: y});
     }
     else
     {
-        points.push({x: x - 700, y: y});
-        points2.push({x: x, y: y});
+        pointsKMeans.push({x: x - 700, y: y});
+        pointsDBSCAN.push({x: x, y: y});
     }
 
-    canva.clearRect(0, 0, canvas.width, canvas.height);
+    cnvs.clearRect(0, 0, canvas.width, canvas.height);
     line();
-    for(let i = 0; i < points.length; i++) 
+    for(let i = 0; i < pointsKMeans.length; i++) 
     {
-        let size_point = document.getElementById("size_point").value;  
-        canva.beginPath();
-        canva.arc(points[i].x, points[i].y, size_point, 0, 2 * Math.PI);
-        canva.fillStyle = "darkslategray";
-        canva.fill();
-
-        canva.beginPath();
-        canva.arc(points[i].x + 700, points[i].y, size_point, 0, 2 * Math.PI);
-        canva.fillStyle = "darkslategray";
-        canva.fill();
+        let sizePoint = receiptSizePoint();
+        cnvs.beginPath();
+        cnvs.arc(pointsKMeans[i].x, pointsKMeans[i].y, sizePoint, 0, 2 * Math.PI);
+        cnvs.arc(pointsKMeans[i].x + 700, pointsKMeans[i].y, sizePoint, 0, 2 * Math.PI);
+        cnvs.fillStyle = "darkslategray";
+        cnvs.fill();
     }
 });
 
 function start_with_clusters()
 {
-    //alert("Не расставляйте центры слишком близко друг к другу иначе алгоритм будет работать не корректно")
+    alert("Не расставляйте центры слишком близко друг к другу иначе алгоритм будет работать не корректно")
 
     canvas.addEventListener('click', function(event)
     {
         let x = event.pageX - canvas.offsetLeft;
         let y = event.pageY - canvas.offsetTop;
         clusters.push({x: x, y: y});
-        points.pop();
+        pointsKMeans.pop();
 
-        let cluster_Index = 0;
-        let count_clusters = document.getElementById("count_clusters").value; 
+        let clusterIndex = 0;
+        let countClusters = document.getElementById("countClusters").value; 
 
         for(let i = 0; i < clusters.length; i++) 
         {
-            canva.beginPath();
-            canva.fillStyle = ["darkred", "darkgreen", "darkblue", "aqua", "purple"][cluster_Index];
-            canva.fillRect(clusters[i].x - 15, clusters[i].y - 15, 30, 30);
-            cluster_Index++;
+            cnvs.beginPath();
+            cnvs.fillStyle = ["darkred", "darkgreen", "darkblue", "aqua", "purple"][clusterIndex];
+            cnvs.fillRect(clusters[i].x - 15, clusters[i].y - 15, 30, 30);
+            clusterIndex++;
         }
 
-        if(cluster_Index == count_clusters)
+        if(clusterIndex == countClusters)
         {
             start();
-            DBSCAN(points2);
+            DBSCAN();
         }
     });
 }
 
 function start() 
 {
-    let count_clusters = document.getElementById("count_clusters").value;
-    let size_point = document.getElementById("size_point").value;
+    let countClusters = document.getElementById("countClusters").value;
+    
 
     for(let iter = 0; iter < 50; iter++) 
     {
         let newClusters = [];
         
-        for(let i = 0; i < count_clusters; i++)
+        for(let i = 0; i < countClusters; i++)
             newClusters.push({x: 0, y: 0, count: 0});
 
-        for(let i = 0; i < points.length; i++) 
+        for(let i = 0; i < pointsKMeans.length; i++) 
         {
             let minDist = Infinity;
             let clusterIndex = 0;
 
-            for(let j = 0; j < count_clusters; j++) 
+            for(let j = 0; j < countClusters; j++) 
             {
-                let dist = Math.sqrt((points[i].x - clusters[j].x) ** 2 + (points[i].y - clusters[j].y) ** 2);
+                let dist = Math.sqrt((pointsKMeans[i].x - clusters[j].x) ** 2 + (pointsKMeans[i].y - clusters[j].y) ** 2);
 
                 if(dist < minDist) 
                 {
@@ -140,17 +143,18 @@ function start()
                     clusterIndex = j;
                 }
             }
-            newClusters[clusterIndex].x += points[i].x;
-            newClusters[clusterIndex].y += points[i].y;
+            newClusters[clusterIndex].x += pointsKMeans[i].x;
+            newClusters[clusterIndex].y += pointsKMeans[i].y;
             newClusters[clusterIndex].count++;
-
-            canva.beginPath();
-            canva.arc(points[i].x, points[i].y, size_point, 0, 2 * Math.PI);
-            canva.fillStyle = ["red", "green", "blue", "aquamarine", "mediumpurple"][clusterIndex];
-            canva.fill();
+            
+            let sizePoint = receiptSizePoint();
+            cnvs.beginPath();
+            cnvs.arc(pointsKMeans[i].x, pointsKMeans[i].y, sizePoint, 0, 2 * Math.PI);
+            cnvs.fillStyle = ["red", "green", "blue", "aquamarine", "mediumpurple"][clusterIndex];
+            cnvs.fill();
         }
         
-        for(let i = 0; i < count_clusters; i++) 
+        for(let i = 0; i < countClusters; i++) 
         {
             if(newClusters[i].count > 0) 
             {
@@ -162,99 +166,98 @@ function start()
     }
 }
 
-let colors = [
-    "#370042",
-    "#000000",
-    "#d180c5",
-    "#9e5493",
-    "#540048",
-    "#483d8b",
-    "#0f129c",
-    "#8A2BE2",
-    "#c71585",
-    "#a658e2",
-    "#d4ace2",
-    "#a497e2",
-    "#7a6ae2",
-    "#e23bcc",
-    "#e200e2"
-]
-
-function DBSCAN(points) 
+function DBSCAN() 
 {
-	let min_count_points = 2;
-	let min_Distance = 20;
-	let size_point = document.getElementById("size_point").value;
-	min_Distance *= size_point / 5;
-    let cluster_ind = 0;
+	let minCountPoints = 2;
+	let minDistance = 20;
+	let sizePoint = receiptSizePoint();
+	minDistance *= sizePoint / 5;
+    let clusterInd = 0;
     let visited = new Set();
-    let cluster = new Array(points.length).fill(-1);
+    let cluster = new Array(pointsDBSCAN.length).fill(-1);
 
-    function cluster_expansion(main_point, cluster_ind) 
+    function cluster_expansion(main_point, clusterInd) 
     {
-        cluster[main_point] = cluster_ind;
+        cluster[main_point] = clusterInd;
         let neighbors = search_neighbors(main_point);
 
-        for (let i = 0; i < neighbors.length; i++) 
+        for (let neigh of neighbors) 
         {
-            let next_point = neighbors[i];
+            let nextPoint = neigh;
 
-            if (!visited.has(next_point)) 
+            if (!visited.has(nextPoint)) 
             {
-                visited.add(next_point);
-                let next_Neighbors = search_neighbors(next_point);
+                visited.add(nextPoint);
+                let next_Neighbors = search_neighbors(nextPoint);
 
-                if (next_Neighbors.length >= min_count_points) 
+                if (next_Neighbors.length >= minCountPoints) 
                     neighbors = neighbors.concat(next_Neighbors);
             }
 
-            if (cluster[next_point] === -1) 
-                cluster[next_point] = cluster_ind;
+            if (cluster[nextPoint] === -1) 
+                cluster[nextPoint] = clusterInd;
         }
     }
     
     function search_neighbors(main_point) 
     {
         let neighbors = [];
-        for (let i = 0; i < points.length; i++) 
-			if (i !== main_point && Math.sqrt((points[i].x - points[main_point].x) ** 2 + (points[i].y - points[main_point].y) ** 2) <= min_Distance)
+        for (let i = 0; i < pointsDBSCAN.length; i++) 
+			if (i !== main_point && Math.sqrt((pointsDBSCAN[i].x - pointsDBSCAN[main_point].x) ** 2 + (pointsDBSCAN[i].y - pointsDBSCAN[main_point].y) ** 2) <= minDistance)
                 neighbors.push(i);
         return neighbors;
     }
   
-    for (let i = 0; i < points.length; i++) 
+    for (let i = 0; i < pointsDBSCAN.length; i++) 
     {
         if (!visited.has(i))
         {
             visited.add(i);
             let neighbors = search_neighbors(i);
 
-            if (neighbors.length < min_count_points) 
+            if (neighbors.length < minCountPoints) 
                 cluster[i] = -1;
             else 
             {
-                cluster[i] = cluster_ind;
-                cluster_expansion(i, cluster_ind);
-                cluster_ind++;
+                cluster[i] = clusterInd;
+                cluster_expansion(i, clusterInd);
+                clusterInd++;
             }
         }
     }
 
     for(let i = 0; i < cluster.length; i++)
     {
+        let colors = [
+            "#FF5733",
+            "#00FFFF",
+            "#FFC300",
+            "#6200EA",
+            "#F7DC6F",
+            "#8E44AD",
+            "#4CAF50",
+            "#E74C3C",
+            "#FFFF00",
+            "#03A9F4",
+            "#FF6F00",
+            "#7F8C8D",
+            "#9B59B6",
+            "#00FF00",
+            "#D35400"
+        ]
         if (cluster[i] !== -1)
         {
-            canva.beginPath();
-            canva.arc(points[i].x, points[i].y, size_point, 0, 2 * Math.PI);
-            canva.fillStyle = colors[cluster[i]];
-            canva.fill();
+            cnvs.beginPath();
+            cnvs.arc(pointsDBSCAN[i].x, pointsDBSCAN[i].y, sizePoint, 0, 2 * Math.PI);
+            cnvs.fillStyle = colors[cluster[i]];
+            cnvs.fill();
         }
         else
         {
-            canva.beginPath();
-            canva.arc(points[i].x, points[i].y, size_point, 0, 2 * Math.PI);
-            canva.fillStyle = "darkslategray";
-            canva.fill();
+            cnvs.beginPath();
+            cnvs.arc(pointsDBSCAN[i].x, pointsDBSCAN[i].y, sizePoint, 0, 2 * Math.PI);
+            cnvs.fillStyle = "darkslategray";
+            cnvs.fill();
         }
     }
 }
